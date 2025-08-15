d074254c91e603336d4e75068a04c67024e00c14
USAishwarya <aishwarya7092004@gmail.com>
2025-08-15 09:52:16 +0530

    Initial Commit

---
+++ src/main/java/com/anoop/vechile/Telimatry/TelimatryApplication.java
@@ -0,0 +1,13 @@
+package com.anoop.vechile.Telimatry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelimatryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelimatryApplication.class, args);
+	}
+
+}
